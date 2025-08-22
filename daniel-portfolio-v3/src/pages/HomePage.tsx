d12c47e10f395dbfb2d5e35df49f709dc25e3381
Daniel <danne.ernas@gmail.com>
2025-08-22 20:39:27 +0200

    Added Portfolio github kanban link

--- daniel-portfolio-v3/src/pages/HomePage.tsx
+++ daniel-portfolio-v3/src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
-import CoomingSoon from "../components/ComingSoon";
-
+import { IoLogoGithub } from "react-icons/io";
+import { BsKanban } from "react-icons/bs";
 function HomePage() {
-  return <CoomingSoon />;
+  return (
+    <div>
+      <h3>Welcome to my Portfolio</h3>
+      <a
+        href="https://github.com/users/LowkiOne/projects/12/views/1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IoLogoGithub /> Github <BsKanban /> Kanban - My Portfolio
+      </a>
+    </div>
+  );
 }
 
 export default HomePage;
