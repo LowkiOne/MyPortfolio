01e1120dadf34ae6aa5f8effe2d6dd88e3dd618c
Daniel <danne.ernas@gmail.com>
2025-08-24 20:05:06 +0200

    fixed some styling

--- daniel-portfolio-v3/src/pages/HomePage.tsx
+++ daniel-portfolio-v3/src/pages/HomePage.tsx
@@ -1,16 +1,36 @@
 import { IoLogoGithub } from "react-icons/io";
 import { BsKanban } from "react-icons/bs";
+import "../styles/homePage.css";
+
 function HomePage() {
   return (
-    <div>
-      <h3>Welcome to my Portfolio</h3>
-      <a
-        href="https://github.com/users/LowkiOne/projects/12/views/1"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <IoLogoGithub /> Github <BsKanban /> Kanban - My Portfolio
-      </a>
+    <div className="home">
+      <div>
+        <h2>Welcome to my Portfolio</h2>
+        <p>
+          My Portfolio is still under development and I work on it every day. I
+          have planned all the frontend and backend, but adding more feauters as
+          I develop.
+        </p>
+        <a
+          href="https://github.com/users/LowkiOne/projects/12/views/1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <IoLogoGithub /> Github <BsKanban /> Kanban - My Portfolio
+        </a>
+      </div>
+      <div>
+        <h3>Plan</h3>
+        <ul>
+          <li>Create frontend website layout</li>
+          <li>Create the frontend layout for all pages</li>
+          <li>Create the frontend pages data</li>
+          <li>Create backend API & database connection</li>
+          <li>Consume the backend API for this react project</li>
+          <li>Create the frontend Admin Page</li>
+        </ul>
+      </div>
     </div>
   );
 }
